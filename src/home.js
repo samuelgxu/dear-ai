4161c8325100d81ebc148cf3e01b6a2e4cb61c8c
Kevin Fang <kfang1233@gmail.com>
2019-02-03 03:58:10 -0500

    Styled pages

--- src/home.js
+++ src/home.js
@@ -8,16 +8,17 @@ class HomePage extends React.Component {
 	render() {
 		return (
 			<div className="HomeGradient" style={{minHeight: '100vh'}}>
-				<div style={{fontSize: "6em", width: '100%', paddingTop: '8vh'}}>
+				<div style={{fontSize: "4em", width: '100%', paddingTop: '8vh'}}>
 					Welcome to Mind AID.
 				</div>
 				<div style={{fontSize: '2em', width: '100%'}}>
-					We use machine learning to <br/>improve your lifestyle and wellbeing</div>
+					We use machine learning to improve your lifestyle and wellbeing</div>
 				<Link to='/addentry'><Button 
 					variant="outlined" 
 					color="primary"
 					style={{marginTop: "20vh", minHeight: '10vh', minWidth: '30vh', fontSize: '1.5em'}} 
-					size="large">Get started</Button></Link>
+					size="large">Get started</Button>
+				</Link>
 			</div>
 		)
 	}
--- src/results.js
+++ src/results.js
@@ -2,47 +2,18 @@ import React, { Component } from 'react';
 import { Button, Hero, Section, Container, Heading, Columns } from "react-bulma-components/full";
 import { Link } from 'react-router-dom';
 
-
 class Results extends React.Component {
 	render() {
 		return (
 			<div class="container">
-				<Section>
-			        <Hero color="info">
-			          <Hero.Body>
-			            <Container>
-			              <Heading size = {1}>Thanks for your entry!</Heading>
-			              <Heading subtitle size={2}>
-			                Based on your journal input, you feel
-			              </Heading>
-			            </Container>
-			          </Hero.Body>
-			        </Hero>
-			      </Section>
-
-			      <Container color>
-				      <Columns>
-				      	<Columns.Column size = "auto">
-				      		<Heading size = {1}>Angry</Heading>
-				      	</Columns.Column>
-				      	<Columns.Column size = "auto">
-				      		<Heading size = {1}>&</Heading>
-				      	</Columns.Column>
-				      	<Columns.Column size = "auto">
-							<Heading size = {1}>Sad</Heading>
-				      	</Columns.Column>
-				      </Columns>
-			      </Container>
-
-			      <Section>
-			        <Hero color="warning">
-			          <Hero.Body>
-			            <Container>
-			              <Heading size = {1}>Because of your mood, we recommend</Heading>			              
-			            </Container>
-			          </Hero.Body>
-			        </Hero>
-			      </Section>
+				<div>
+					<div style={{marginTop: 10, fontSize: '2em', backgroundColor: 'light-blue'}}>
+						Based on your journal entry, I think you feel:<br/>
+						<span style={{fontSize: '2.2em'}}> happy </span>
+						and a little 
+						<span style={{fontSize: '2.2em'}}> scared </span> 
+					</div>
+				</div>
 			</div>
 		);
 	}
